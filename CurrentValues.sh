253643e82778b8f1bd6dbdf75999b074e8a9f49c
Ashwath <90554655+asphylis@users.noreply.github.com>
2022-09-29 20:11:19 +0530

    Create CurrentValues.sh
    Write a Shell Scripting program to display the Current Date, Time, User, and Directory.

---
+++ CurrentValues.sh
@@ -0,0 +1,6 @@
+# Shell Script to display the Current Date, Time, User, and Directory
+
+echo "Current Date: "`date +"%a %b %d %Y"`
+echo "Current Time: "`date +"%H:%M:%S %Z"`
+echo "Current User: `whoami`"
+echo "Current Directory: `pwd`"
